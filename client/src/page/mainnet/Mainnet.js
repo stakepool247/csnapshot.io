5df4223b2b7f154b3c333ff4ee1f7f3c59aa60e7
StakePool247 <59128747+stakepool247@users.noreply.github.com>
2022-04-23 22:59:15 +0300

    loading data from json url

--- client/src/page/mainnet/Mainnet.js
+++ client/src/page/mainnet/Mainnet.js
@@ -1,57 +1,16 @@
-import React from 'react'
 import './Mainnet.css'
-import MainnetRecords from "../../../public/mainnet.json"
-fetch('data/mainnet.json')
-
+import JsonTable from '../../components/JsonTable'
 const Mainnet = () => {
   return (
     <div>
+      
+
   <div className='snapshot-container'>
         <div className="card-frame">   
-    <h1>Mainnet Snapshot</h1>
-   
-<table>
-  <tr>
-    <th>Cardano Chain</th>
-    <th>Mainnet</th>
-  </tr>
-  <tr>
-    <td>Height</td>
-    <td>{MainnetRecords.height}</td>
-  </tr>
-  <tr>
-    <td>Hash</td>
-    <td>{MainnetRecords.hash}</td>
-  </tr>
-  <tr>
-    <td>slot</td>
-    <td>{MainnetRecords.slot}</td>
-  </tr>
-  <tr>
-    <td>Epoch</td>
-    <td>{MainnetRecords.epoch}</td>
-  </tr>
-  <tr>
-    <td>compresion format</td>
-    <td>lz4</td>
-  </tr>
-  <tr>
-    <td>DB Size (uncompressed)</td>
-    <td>54 Gb</td>
-  </tr>
-  <tr>
-    <td>Archive size:</td>
-    <td>32 Gb</td>
-  </tr>
-  <tr>
-    <td>Download link:</td>
-    <td>direct download</td>
-  </tr>
-</table>
-
+        <JsonTable />
 <h3>Quick way to download to your server and extract files: </h3>
-<div class="highlight">
-  <pre class="highlight">
+<div className="highlight">
+  <pre className="highlight">
     <code>curl -o -L https://cSnapshots.io/mainnet.tar.lz4 | lz4 -c -d - | tar -x -C /home/cardano/cnode/db/ </code>
 </pre></div>
 
