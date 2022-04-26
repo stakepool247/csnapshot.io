922728d1fac298dc0a81b5a897a21000b652e2b5
StakePool247 <59128747+stakepool247@users.noreply.github.com>
2022-04-26 12:02:27 +0300

    converted json loader to JSX

--- client/src/page/mainnet/Mainnet.js
+++ client/src/page/mainnet/Mainnet.js
@@ -1,13 +1,14 @@
-import './Mainnet.css'
-import JsonTable from '../../components/JsonTable'
+import './Mainnet.css';
+// import JsonTable from '../../components/JsonTable'
+import SnapshotTable from '../../components/SnapshotTable';
 const Mainnet = () => {
-  return (
-  <div className='snapshot-container'>
-        <div className="card-frame">   
-        <JsonTable />
-  </div>
-</div>
-  )
-}
+	return (
+		<div className='snapshot-container'>
+			<div className='card-frame'>
+				<SnapshotTable />
+			</div>
+		</div>
+	);
+};
 
-export default Mainnet
+export default Mainnet;
