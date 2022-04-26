dc0eb51f85e54612de154f86ec6eef4d57c8764e
StakePool247 <59128747+stakepool247@users.noreply.github.com>
2022-04-26 22:53:32 +0300

    changed the json data location due to caching problems

--- client/src/page/mainnet/Mainnet.js
+++ client/src/page/mainnet/Mainnet.js
@@ -1,11 +1,11 @@
 import './Mainnet.css';
-// import JsonTable from '../../components/JsonTable'
 import SnapshotTable from '../../components/SnapshotTable';
 const Mainnet = () => {
 	return (
 		<div className='snapshot-container'>
 			<div className='card-frame'>
-				<SnapshotTable />
+				<h1>The Cardano Mainnet Blockchain DB</h1>
+				<SnapshotTable chain='mainnet' />
 			</div>
 		</div>
 	);
