4fc9c47c8127b3f9fad57d5e168142bc37ec758f
StakePool247 <59128747+stakepool247@users.noreply.github.com>
2022-05-03 13:40:10 +0300

    Make repository public

--- client/src/views/home/Hero.jsx
+++ client/src/views/home/Hero.jsx
@@ -1,27 +1,28 @@
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import Stack from '@mui/material/Stack'
-import Card from '../../components/common/Card'
-import HeroLayout from '../../components/common/HeroLayout'
-import { appsDetails } from '../../helper/common'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Card from '../../components/common/Card';
+import HeroLayout from '../../components/common/HeroLayout';
+import { appsDetails } from '../../helper/common';
 
 export default function Hero() {
 	return (
-		<Box component="section">
+		<Box component='section'>
 			<HeroLayout
 				sx={{
 					minHeight: 300,
 					py: 10,
-					pb: { xs: theme => `calc(190px + ${theme.spacing(5)})`, md: 27 },
+					pb: { xs: (theme) => `calc(190px + ${theme.spacing(5)})`, md: 27 },
 				}}
 			>
-				<Typography textAlign="center" variant="h1" marginBottom={3}>
-					Download latest cardano-node{' '}
-					<Box component="br" sx={{ display: { xs: 'none', md: 'block' } }} /> snapshot DB
-					files
+				<Typography textAlign='center' variant='h1' marginBottom={3}>
+					Download the latest Cardano{' '}
+					<Box component='br' sx={{ display: { xs: 'none', md: 'block' } }} />{' '}
+					blockchain snapshot
 				</Typography>
-				<Typography textAlign="center" variant="h5">
-					so you can get your node up and running faster! We are updating snapshots daily!
+				<Typography textAlign='center' variant='h5'>
+					We are updating the snapshots daily, so you can sync your Cardano node
+					much faster!
 				</Typography>
 			</HeroLayout>
 			<Stack
@@ -40,5 +41,5 @@ export default function Hero() {
 				))}
 			</Stack>
 		</Box>
-	)
+	);
 }
