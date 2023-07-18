88448d131983ff1f684befe1046ba36e44a6c8c2
StakePool247 <59128747+stakepool247@users.noreply.github.com>
2023-07-18 17:57:13 +0300

    Make repository public

---
+++ src/views/support/index.jsx
@@ -0,0 +1,20 @@
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Hero from './Hero';
+import SupportUs from './SupportUs';
+import { useEffect } from 'react';
+
+export default function Mainnet() {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	return (
+		<Box component='main'>
+			<Hero />
+			<Container maxWidth='xl'>
+				<SupportUs />
+			</Container>
+		</Box>
+	);
+}
