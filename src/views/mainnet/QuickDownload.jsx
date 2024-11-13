9cc5d61480f43f6d9a3f7cbd3f7409337c2a527f
stakepool247 <lauris@stakepool247.eu>
2024-11-13 21:35:45 +0200

    updating tools needed and extra step

--- src/views/mainnet/QuickDownload.jsx
+++ src/views/mainnet/QuickDownload.jsx
@@ -7,37 +7,49 @@ import { terminalContainer } from '../../components/common/styles';
 export default function QuickDownload() {
 	return (
 		<Box component='section' sx={{ mt: 5 }}>
-		<Alert severity='success' sx={{ mb: 4, fontSize: '1.2rem', bgcolor: 'primary.main', color: 'white', p: 2 }}>
-			<AlertTitle>Important Update</AlertTitle>
-			We have updated the compression method to use <strong>Zstandard (zstd)</strong>, which reduces the download size by half! Please double-check to make sure you are not using the old command with <strong>lz4</strong>.
-		</Alert>
+								<Box sx={terminalContainer} marginBottom={3}>
+					<Alert severity='success' marginBottom={3} marginTop={1} sx ={{bgcolor: 'primary.main', color: 'white'}}>
+						<AlertTitle>Warning</AlertTitle>
+						We have updated the compression method to use <strong>Zstandard (zstd)</strong>, which reduces the download size by half! Please double-check to make sure you are not using the old command with <strong>lz4</strong>.
+						</Alert>
+				</Box>
 			<Box component='section' sx={{ mt: 5 }}>
 				<Typography variant='h3' marginBottom={3}>
 					Quick way to download snapshot to your server and extract files
 				</Typography>
 				<Typography variant='h4' marginBottom={3} marginTop={2}>
-					1. Set the environment variable for the target directory:
+					1. Set the environment variable for the target directory or change the path to your desired location:
 				</Typography>
 				<Terminal
 					sx={{ mt: 2 }}
-					cmd='export TARGET_DIR=/home/cardano/cnode/'
+					cmd='export TARGET_DIR=/home/cardano/cnode'
 				/>
 				<Typography variant='h4' marginBottom={3} marginTop={2}>
-					2. Update / install curl, jq and zstd tool:
+					2. Update / install required tools (jq, zstd and wget):
 				</Typography>
 				<Terminal
 					sx={{ mt: 2 }}
-					cmd='sudo apt update && sudo apt install zstd jq curl'
+					cmd='sudo apt update && sudo apt install zstd jq wget -y'
 				/>
-				<Typography variant='h4' marginBottom={3} marginTop={2}>
-					3. Download and extract the files:
+				
+<Typography variant='h4' marginBottom={3} marginTop={2}>
+					3. Remove the old files and create target folder:):
 				</Typography>
 				<Box sx={terminalContainer} marginBottom={3}>
 					<Alert severity='warning' marginBottom={3} marginTop={1}>
 						<AlertTitle>Warning</AlertTitle>
-						This will add/replace the data in the target directory specified by $TARGET_DIR
+						This will clean the db folder in the target directory specified by $TARGET_DIR
 					</Alert>
 				</Box>
+				<Terminal
+					sx={{ mt: 2 }}
+					cmd='rm -rf $TARGET_DIR/db/* && mkdir -p $TARGET_DIR/db'
+				/>
+
+				<Typography variant='h4' marginBottom={3} marginTop={2}>
+					4. Download and extract the files:
+				</Typography>
+
 				<Terminal
 					cmd='wget -c -O - "https://downloads.csnapshots.io/mainnet/$(wget -qO- https://downloads.csnapshots.io/mainnet/mainnet-db-snapshot.json | jq -r .[].file_name)" | zstd -d -c | tar -x -C $TARGET_DIR/'
 				/>
